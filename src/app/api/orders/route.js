0ff534bc802c5cb21ec3f79f1c4bae0a9860088c
Anirudh Sriram <srirama@cs.uchicago.edu>
2024-06-13 13:27:27 +0200

    updates

--- src/app/api/orders/route.js
+++ src/app/api/orders/route.js
@@ -1,4 +1,5 @@
-import { authOptions, isAdmin } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import isAdmin from "@/libs/isAdmin";
 import { Order } from "@/app/models/Order";
 import mongoose from "mongoose";
 import { getServerSession } from "next-auth";
---
+++ src/libs/isAdmin.js
@@ -0,0 +1,15 @@
+import { UserInfo } from "@/app/models/UserInfo";
+import NextAuth, { getServerSession } from "next-auth";
+
+export default async function isAdmin() {
+  const session = await getServerSession(authOptions);
+  const userEmail = session?.user?.email;
+  if (!userEmail) {
+    return false;
+  }
+  const userInfo = await UserInfo.findOne({ email: userEmail });
+  if (!userInfo) {
+    return false;
+  }
+  return userInfo.admin;
+}
